33c417566a03ce711fcbe935408e72ddf9ba87c8
Alexander Harkness <bearbin@gmail.com>
2014-01-30 20:58:14 +0000

    Improved the breaking and placing logic massively.

--- OnBreakPlaceBlock.lua
+++ OnBreakPlaceBlock.lua
@@ -21,72 +21,53 @@
 
 
 function OnPlayerPlacingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, CursorY, CursorZ, BlockType)
-        -- Don't fail if the block is air.
-        if (BlockFace == -1) then
-                return false
-        end
 
-        local PROTECTRADIUS = WorldsSpawnProtect[Player:GetWorld():GetName()];
+	-- Don't fail if the block is air.
+	if BlockFace == -1 then
+		return false
+	end
 
-        if not (Player:HasPermission("core.build")) then
-                SendMessageFailure( Player, "You do not have the 'core.build' permission, thou cannot build" )
-                return true
-        end
-        
-        if not (Player:HasPermission("core.spawnprotect.bypass")) and (PROTECTRADIUS ~= 0) then
-                local World = Player:GetWorld()
-                local xcoord = World:GetSpawnX()
-                local ycoord = World:GetSpawnY()
-                local zcoord = World:GetSpawnZ()
-                if not ((BlockX <= (xcoord + PROTECTRADIUS)) and (BlockX >= (xcoord - PROTECTRADIUS))) then
-                        return false -- Not in spawn area.
-                end
-                if not ((BlockY <= (ycoord + PROTECTRADIUS)) and (BlockY >= (ycoord - PROTECTRADIUS))) then 
-                        return false -- Not in spawn area.
-                end
-                if not ((BlockZ <= (zcoord + PROTECTRADIUS)) and (BlockZ >= (zcoord - PROTECTRADIUS))) then 
-                        return false -- Not in spawn area.
-                end
-                        SendMessageFailure( Player, "Go further from spawn to build" )
-                        return true
-        end
-        
-        return false
+	-- Does the player have permission to bypass the spawn protection?        
+	if Player:HasPermission("spawnprotect.bypass") then
+		return false
+	end
+
+	local world = Player:GetWorld():GetName()
+	local xcoord = World:GetSpawnX()
+	local ycoord = World:GetSpawnY()
+	local zcoord = World:GetSpawnZ()
+
+	if IsInSpawn(xcoord, ycoord, zcoord)
+		SendMessageInfo(Player, "Go further from spawn to build")
+		return true
+	end
+	
+	return false
+
 end
 
 function OnPlayerBreakingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, Status, OldBlockType, OldBlockMeta)
-        -- Don't fail if the block is air.
-        if BlockFace == -1 then
-                return false
-        end
 
-        local PROTECTRADIUS = WorldsSpawnProtect[Player:GetWorld():GetName()];
+	-- Don't fail if the block is air.
+	if BlockFace == -1 then
+		return false
+	end
 
-        if not (Player:HasPermission("core.build")) then
-                SendMessageFailure( Player, "You do not have the 'core.build' permission, thou cannot build" )
-                return true
-        end
-        
-        if not (Player:HasPermission("core.spawnprotect.bypass")) and not (PROTECTRADIUS == 0) then
-                local World = Player:GetWorld()
-                local xcoord = World:GetSpawnX()
-                local ycoord = World:GetSpawnY()
-                local zcoord = World:GetSpawnZ()
+	-- Does the player have permission to bypass the spawn protection?        
+	if Player:HasPermission("spawnprotect.bypass") then
+		return false
+	end
 
-                if not ((BlockX <= (xcoord + PROTECTRADIUS)) and (BlockX >= (xcoord - PROTECTRADIUS))) then
-                        return false -- Not in spawn area.
-                end
-                if not ((BlockY <= (ycoord + PROTECTRADIUS)) and (BlockY >= (ycoord - PROTECTRADIUS))) then 
-                        return false -- Not in spawn area.
-                end
-                if not ((BlockZ <= (zcoord + PROTECTRADIUS)) and (BlockZ >= (zcoord - PROTECTRADIUS))) then 
-                        return false -- Not in spawn area.
-                end
-        
-                SendMessageFailure( Player, "Go further from spawn to build" )
+	local world = Player:GetWorld():GetName()
+	local xcoord = World:GetSpawnX()
+	local ycoord = World:GetSpawnY()
+	local zcoord = World:GetSpawnZ()
 
-                return true
-        end
-
-        return false
+	if IsInSpawn(xcoord, ycoord, zcoord)
+		SendMessageInfo(Player, "Go further from spawn to build")
+		return true
+	end
+	
+	return false
+	
 end
