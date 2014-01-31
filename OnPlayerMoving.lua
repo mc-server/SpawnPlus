75a4625bfa9de2b682e7a243e1635d55ff8e842d
Alexander Harkness <bearbin@gmail.com>
2014-01-31 19:07:59 +0000

    Did some more.

--- OnPlayerMoving.lua
+++ OnPlayerMoving.lua
@@ -19,6 +19,11 @@
 -- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 -- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+-- Globals
+PLAYERLOCATIONS = {}
+
+-- Code Start
+
 function OnPlayerMoving(Player)
 
 	local world = Player:GetWorld():GetName()
